62ee0a9ecca257e77f29c9049d3b8a1cdc5ba7fa
devumairazmat <umairazmat8260@gmail.com>
2022-10-24 22:33:44 +0500

    connected to db

--- server/app.js
+++ server/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+// dotenv
+require("dotenv").config();
+// Mongoose
+const mongoose = require("mongoose");
 
 // Middleware configuration
 
 const middleware = (req, res, next) => {
   console.log("Middleware configuration");
-  next();
 };
 
 app.get("/", (req, res) => {
@@ -29,6 +32,14 @@ app.get("/signup", (req, res) => {
   res.send("Welcome to  MERN thapa Regestration");
 });
 
-app.listen("3000", () => {
-  console.log("listening on http://localhost:3000");
-});
+// Connect To mongo Db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen("3000", () => {
+      console.log("listening on http://localhost:3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
