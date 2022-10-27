facc0b32532f8c01aa18e60718a05cdd5a5f23d4
devumairazmat <umairazmat8260@gmail.com>
2022-10-27 21:53:08 +0500

    diff changes done

--- server/app.js
+++ server/app.js
@@ -1,28 +1,38 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // dotenv
 const dotenv = require("dotenv");
 // require("dotenv").config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const PORT = process.env.PORT;
 dotenv.config({ path: "./config.env" });
+
+// Db connect
 require("./db/conn");
+// Schema Connect
 require("./model/userSchema");
+// Route auth Connect
+app.use(require("./router/auth"));
 // Middleware configuration
 
 const middleware = (req, res, next) => {
   console.log("Middleware configuration");
+  next();
 };
 
 app.get("/", (req, res) => {
-  res.send("Welcome to  MERN thapa Home");
-  next();
+  res.send("Welcome to  MERN thapa Home from app");
 });
 
-app.get("/about", middleware, (req, res) => {
+app.get("/about", (req, res) => {
   res.send("Welcome to  MERN thapa About");
 });
 
-app.get("/contact", middleware, (req, res) => {
+app.get("/contact", (req, res) => {
   res.send("Welcome to  MERN thapa Contact");
 });
 
@@ -32,4 +42,8 @@ app.get("/signin", (req, res) => {
 
 app.get("/signup", (req, res) => {
   res.send("Welcome to  MERN thapa Regestration");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Connecting to DB & App listening on port" + process.env.PORT);
 });
