aecd5062d8a5b855a3b4bd8b29af447f6fee6041
devumairazmat <umairazmat8260@gmail.com>
2022-10-29 20:55:59 +0500

    User Regestration Done Data Stored in DB

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,4 +1,6 @@
+const { response } = require("express");
 const express = require("express");
+const User = require("../model/userSchema");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -7,8 +9,30 @@ router.get("/", (req, res) => {
 });
 
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.json({ message: req.body });
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "Please Fill the Form Properly" });
+  }
+
+  User.findOne({ email: email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({ error: "Email already exists" });
+      }
+
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      user.save().then(() => {
+          res.status(201).json({ message: "User Registred successfully" });
+        })
+        .catch((err) => {
+          res.status(500).json({ err: "User Regestration Failed" });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
